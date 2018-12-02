92ba9e319be8ce0912223be795c39344ddf921c0
vikas singh <vikas.singh@easygov.co.in>
2018-12-03 01:26:52 +0530

    updated api

--- src/api/index.js
+++ src/api/index.js
@@ -18,9 +18,7 @@ export const getReposApi = (userName, {searchTerm='', type='fork:true', language
     const term = searchTerm && searchTerm.length ? `${searchTerm}+` : '';
     let repoType = `+${type}`;
     return new Promise((resolve, reject) => {
-        fetch(`${baseURL}/search/repositories?q=${term}user:${userName}+language:${language}${repoType}&sort=stars&order=desc&page=1&per_page=100`, {headers:{
-            Authorization: `token 2d8be39836ed34d5b9ed36519d3adcb42b45d1ff`
-        }})
+        fetch(`${baseURL}/search/repositories?q=${term}user:${userName}+language:${language}${repoType}&sort=stars&order=desc&page=1&per_page=100`)
             .then(response => {
                 if (response.ok) {
                     resolve(response.json());
